34235f9863da814ba4cfe535eebf69730833887d
L3iHS <ilyadroscov@gmail.com>
2025-12-17 00:55:04 +0300

    add chip.py

---
+++ src/models/chip.py
@@ -0,0 +1,24 @@
+class Chip:
+    def __init__(self, value: int) -> None:
+        self.value = max(1, int(value))  # значение фишки должно быть >=1
+    
+    def __repr__(self) -> str:
+        return f'Chip(value={self.value})'
+    
+    def __add__(self, other: 'Chip') -> 'Chip':  # суммирование фишек
+        return Chip(self.value + other.value)
+    
+    def __radd__(self, other: 'Chip') -> 'Chip':
+        '''
+        нужен если левый операнд не поддерживает сложение (__add__),
+        то вызовется __radd__ правого операнда
+        например при sum(), когда первый элемент 0 (int)
+        '''
+        if other == 0:
+            return self
+        
+        if isinstance(other, Chip):
+            return self.__add__(other)
+
+        # дальше питон попробует другие варианты и если нужно, выдаст ошибку
+        return NotImplemented
