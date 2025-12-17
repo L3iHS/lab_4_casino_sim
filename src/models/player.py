bb3ba3986ed35e86609dd25b07c7b92c74f2c3c1
L3iHS <ilyadroscov@gmail.com>
2025-12-17 16:40:26 +0300

    update player.py

--- src/models/player.py
+++ src/models/player.py
@@ -1,8 +1,11 @@
 class Player:
     def __init__(self, name: str, balance: int) -> None:
-        self.name = name
-        self.balance = balance
-    
+        if not isinstance(name, str) or not name:
+            self.name = "player"
+        else:
+            self.name = name
+        self.balance = max(1, int(balance))  # баланс должен быть >=1
+
     def __repr__(self) -> str:
         # !r явно показывает экранирование,
         # оставляет например /n для сложных имен
